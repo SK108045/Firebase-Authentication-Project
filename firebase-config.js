086babcdb03e1b9881eb40eb0ec999fe497fddcb
sk10 <124081032+SK108045@users.noreply.github.com>
2024-09-25 20:48:30 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Your Firebase configuration
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
